c96c77a4374b6b6dc7bb0ccf79f00dddb32b0281
Lubomir Olshansky <ol.lub.ua@gmail.com>
2018-06-23 00:12:27 +0200

    stack view to main vc added

---
+++ Night_Club_Application/Resources/MainViewButton.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewButton.swift
+//  Night_Club_Application
+//
+//  Created by Lubomir Olshansky on 22/06/2018.
+//  Copyright © 2018 Lubomir Olshansky. All rights reserved.
+//
+
+import UIKit
+
+class MainViewButton: UIButton {
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        layer.borderWidth = 1
+        layer.borderColor = UIColor(red: 150, green: 150, blue: 150, alpha: 1).cgColor
+    }
+
+}
