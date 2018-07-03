0e91234f55a8ea2118e36c6a37d959724aa5b74a
Lubomir Olshansky <ol.lub.ua@gmail.com>
2018-07-04 00:18:54 +0200

    club cards view controller added

---
+++ Night_Club_Application/Controllers/ClubCardsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ClubCardsViewController.swift
+//  Night_Club_Application
+//
+//  Created by Lubomir Olshansky on 04/07/2018.
+//  Copyright © 2018 Lubomir Olshansky. All rights reserved.
+//
+
+import UIKit
+
+class ClubCardsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+   
+}
