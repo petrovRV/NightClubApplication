aafc84407d700b06037a137b9011768dc235e079
Lubomir Olshansky <ol.lub.ua@gmail.com>
2018-07-04 17:59:13 +0200

    alamofire request added

---
+++ Night_Club_Application/Models/Videos.swift
@@ -0,0 +1,13 @@
+//
+//  Videos.swift
+//  Night_Club_Application
+//
+//  Created by Lubomir Olshansky on 04/07/2018.
+//  Copyright © 2018 Lubomir Olshansky. All rights reserved.
+//
+
+import Foundation
+
+struct Videos: Decodable {
+    
+}
--- Night_Club_Application/Services/VideosNetworkService.swift
+++ Night_Club_Application/Services/VideosNetworkService.swift
@@ -12,8 +12,33 @@ import Alamofire
 
 class VideosNetwrokService {
     
+    typealias loadVideosDataCompletion = ([Videos]) -> Void
+    
     let baseUrl = "https://www.googleapis.com"
     let apiKey = "AIzaSyAfsBcF5oRVZ1gaDDMAht_aAk-CQR-9s3Q"
+    let channelId = "UCEJNQ2nuAA-R9vpPZ22Y2Cg"
     
-    
+    func loadVideosData(completion: @escaping loadVideosDataCompletion ){
+        
+        let path = "/youtube/v3/search"
+        let parameters: Parameters = [
+            "part": "snippet",
+            "channelId": channelId,
+            "key": apiKey
+        ]
+        let url = baseUrl + path
+
+        Alamofire.request(url, method: .get, parameters: parameters).responseJSON
+            { respons in
+            guard let data = respons.data else {return}
+            
+            do {
+                let videosDecoded = try JSONDecoder().decode(Videos.self, from: data)
+                completion([videosDecoded])
+            return
+        } catch let jsonErr {
+            print("Error serializing json:", jsonErr)
+            }
+        }
+    }
 }
