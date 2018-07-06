01672c5820c95e0c14b8ccc66e0bbba88d1ee418
Lubomir Olshansky <ol.lub.ua@gmail.com>
2018-07-06 21:41:45 +0200

    selector to Segmented Control added

--- Night_Club_Application/Services/CustomSegmentedControl.swift
+++ Night_Club_Application/Services/CustomSegmentedControl.swift
@@ -13,6 +13,7 @@ import UIKit
 class CustomSegmentedControl: UIView {
     
     var buttons = [UIButton]()
+    var selector: UIView!
     
     @IBInspectable
     var borderWidth: CGFloat = 0 {
@@ -35,30 +36,69 @@ class CustomSegmentedControl: UIView {
         }
     }
     
+    @IBInspectable
+    var textColor: UIColor = .lightGray {
+        didSet {
+            updateView()
+        }
+    }
+    
+    @IBInspectable
+    var selectorColor: UIColor = .darkGray {
+        didSet {
+            updateView()
+        }
+    }
+    @IBInspectable
+    var selectorTextColor: UIColor = .white {
+        didSet {
+            updateView()
+        }
+    }
+
     func updateView() {
         buttons.removeAll()
-        
+        subviews.forEach { $0.removeFromSuperview() }
         let buttonsTitles = commaSeparatedButtonTitles.components(separatedBy: ",")
         
         for buttonTitle in buttonsTitles {
             let button = UIButton(type: .system)
             button.setTitle(buttonTitle, for: .normal)
+            button.setTitleColor(textColor, for: .normal)
+            button.addTarget(self, action: #selector(buttonTapped(button:)), for: .touchUpInside)
             buttons.append(button)
         }
+        
+        
+        buttons[0].setTitleColor(selectorTextColor, for: .normal)
+        
+        let selectorWidth = frame.width / CGFloat(buttonsTitles.count)
+        selector = UIView(frame: CGRect(x: 0, y: 0, width: selectorWidth, height: frame.height))
+        selector.layer.cornerRadius = frame.height / 2
+        selector.backgroundColor = selectorColor
+        addSubview(selector)
         
         let stackView = UIStackView(arrangedSubviews: buttons)
         stackView.axis = .horizontal
         stackView.alignment = .fill
         stackView.distribution = .fillEqually
+        addSubview(stackView)
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
         stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
         stackView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
         stackView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
-        addSubview(stackView)
+    
     }
-    
-    
+
+    @objc func buttonTapped(button: UIButton) {
+        for btn in buttons {
+            btn.setTitleColor(textColor, for: .normal)
+            if btn == button {
+                btn.setTitleColor(selectorTextColor, for: .normal)
+            }
+        }
+    }
     
     
     override func draw(_ rect: CGRect) {
