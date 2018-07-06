510a27d61e419b008bfca0e6d3ef4c21a1a6588e
Lubomir Olshansky <ol.lub.ua@gmail.com>
2018-07-06 17:50:15 +0200

    buttons and StackView added

--- Night_Club_Application/Services/CustomSegmentedControl.swift
+++ Night_Club_Application/Services/CustomSegmentedControl.swift
@@ -12,6 +12,8 @@ import UIKit
 @IBDesignable
 class CustomSegmentedControl: UIView {
     
+    var buttons = [UIButton]()
+    
     @IBInspectable
     var borderWidth: CGFloat = 0 {
         didSet {
@@ -25,6 +27,38 @@ class CustomSegmentedControl: UIView {
             layer.borderColor = borderColor.cgColor
         }
     }
+    
+    @IBInspectable
+    var commaSeparatedButtonTitles: String = "" {
+        didSet {
+            updateView()
+        }
+    }
+    
+    func updateView() {
+        buttons.removeAll()
+        
+        let buttonsTitles = commaSeparatedButtonTitles.components(separatedBy: ",")
+        
+        for buttonTitle in buttonsTitles {
+            let button = UIButton(type: .system)
+            button.setTitle(buttonTitle, for: .normal)
+            buttons.append(button)
+        }
+        
+        let stackView = UIStackView(arrangedSubviews: buttons)
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        stackView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        stackView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        addSubview(stackView)
+    }
+    
+    
     
     
     override func draw(_ rect: CGRect) {
