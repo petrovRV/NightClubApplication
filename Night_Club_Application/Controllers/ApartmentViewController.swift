a17b1ccd310fcba38c8fae9c7486dee57ea6cb0f
Lubomir Olshansky <ol.lub.ua@gmail.com>
2018-07-11 20:31:42 +0200

    page control to apartments photos collection view implemented

--- Night_Club_Application/Controllers/ApartmentViewController.swift
+++ Night_Club_Application/Controllers/ApartmentViewController.swift
@@ -10,12 +10,15 @@ import UIKit
 
 class ApartmentViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout{
   
+    @IBOutlet weak var apartmentPhotosCollectionView: UICollectionView!
+    @IBOutlet weak var pageControl: UIPageControl!
     var apartmentPhotos = [#imageLiteral(resourceName: "apartament1"), #imageLiteral(resourceName: "apartament2"), #imageLiteral(resourceName: "apartament3"), #imageLiteral(resourceName: "apartament4"), #imageLiteral(resourceName: "apartament5")]
 
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        pageControl.numberOfPages = apartmentPhotos.count
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -38,6 +41,16 @@ class ApartmentViewController: UIViewController, UICollectionViewDataSource, UIC
 
         
         return cell
+    }
+    func  scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        
+        let layout = self.apartmentPhotosCollectionView?.collectionViewLayout as! UICollectionViewFlowLayout
+        let cellWidthIncludingSpacing = layout.itemSize.width + layout.minimumLineSpacing
+        var offset = targetContentOffset.pointee
+        let index = (offset.x + scrollView.contentInset.left) / cellWidthIncludingSpacing
+        let roundedIndex = round(index)
+        self.pageControl.currentPage = Int(roundedIndex)
+       
     }
 //    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
 //        let height = collectionView.frame.height
