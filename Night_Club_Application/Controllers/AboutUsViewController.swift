6badd2bdfac2fd9984dae12e4502115a04fab2b8
Lubomir Olshansky <ol.lub.ua@gmail.com>
2018-07-13 16:20:23 +0200

    about us view controller and storyboard created

---
+++ Night_Club_Application/Controllers/AboutUsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AboutUsViewController.swift
+//  Night_Club_Application
+//
+//  Created by Lubomir Olshansky on 13/07/2018.
+//  Copyright © 2018 Lubomir Olshansky. All rights reserved.
+//
+
+import UIKit
+
+class AboutUsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
